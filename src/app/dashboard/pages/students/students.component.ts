a534ac910bb0e217a98808049e987b1e255ea5d6
CENTRAL\sberteloot <sberteloot@coto.com.ar>
2023-07-16 12:52:37 -0300

    Limpio datos de prueba

--- src/app/dashboard/pages/students/students.component.ts
+++ src/app/dashboard/pages/students/students.component.ts
@@ -11,12 +11,7 @@ import { ConfirmdialogComponent } from 'src/app/shared/components/confirmdialog/
 })
 export class StudentsComponent {
   
-  arrayStudents: IStudent[] = [
-    {id: 1, name: 'sergio', surname: 'berteloot', email: 'sjberteloot@gmail.com', birth :new Date("1977-03-09")},
-    {id: 2, name: 'jose', surname: 'gonzalez', email: 'jose.gonzalez@gmail.com', birth :new Date("2001-03-05")},
-    {id: 3, name: 'ana', surname: 'lopez', email: 'ana.lopez@gmail.com', birth :new Date("2003-05-07")},
-    {id: 4, name: 'maria', surname: 'sanchez', email: 'maria.sanchez@gmail.com', birth :new Date("1999-12-21")}
-  ];
+  arrayStudents: IStudent[] = [];
 
   constructor(private studentDialog : MatDialog, 
               private confirmDialog: MatDialog){}
