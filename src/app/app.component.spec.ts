fc62ab45d8ff6867c15457f111f6c4f70a7d1dae
CENTRAL\sberteloot <sberteloot@coto.com.ar>
2023-07-21 19:24:25 -0300

    Cambio a 2pf

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '1pf_Berteloot'`, () => {
+  it(`should have as title '2pf_Berteloot'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('1pf_Berteloot');
+    expect(app.title).toEqual('2pf_Berteloot');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('1pf_Berteloot app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('2pf_Berteloot app is running!');
   });
 });
