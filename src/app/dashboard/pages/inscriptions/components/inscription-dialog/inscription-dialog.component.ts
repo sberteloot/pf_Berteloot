aacc846a930311d350d9b5fadceb6e34c5a87b9a
CENTRAL\sberteloot <sberteloot@coto.com.ar>
2023-08-20 10:00:15 -0300

    ADD, Dialog de Inscripcion

--- src/app/dashboard/pages/inscriptions/components/inscription-dialog/inscription-dialog.component.ts
+++ src/app/dashboard/pages/inscriptions/components/inscription-dialog/inscription-dialog.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { IStudent } from '../../../students/models/student';
+import { Observable } from "rxjs";
+import { ICourse } from '../../../courses/models/course';
+import { StudentsService } from '../../../students/students.service';
+import { CoursesService } from '../../../courses/courses.service';
 
 @Component({
   selector: 'app-inscription-dialog',
@@ -6,5 +13,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./inscription-dialog.component.scss']
 })
 export class InscriptionDialogComponent {
+
+  studentIdFormControl = new FormControl<number | null>(null, [Validators.required]);
+  courseIdFormControl = new FormControl<number | null>(null, [Validators.required]);
+
+  inscriptionFormGroup = new FormGroup({
+    studentId : this.studentIdFormControl,
+    courseId : this.courseIdFormControl
+  })
+
+  students$ : Observable<IStudent[]>;
+  courses$ : Observable<ICourse[]>;
+
+  constructor(private dialogRef: MatDialogRef<InscriptionDialogComponent>,
+              private studentsService: StudentsService,
+              private coursesService: CoursesService){
+    this.studentsService.loadStudents();
+    this.students$ = this.studentsService.getStudents();
+
+    this.coursesService.loadCourses();
+    this.courses$ = this.coursesService.getCourses();
+  }
+
+  clearForm() : void{
+    this.inscriptionFormGroup.reset();
+  }
+
+  closeDialog(){
+    this.dialogRef.close();
+  }
+
+  enviarDialog() : void{
+
+  }
 
 }
--- src/app/dashboard/pages/inscriptions/inscriptions.component.ts
+++ src/app/dashboard/pages/inscriptions/inscriptions.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { InscriptionActions } from './store/inscription.actions';
 import { IInscription } from './models/inscription';
 import { selectInscriptions } from './store/inscription.selectors';
+import { MatDialog } from '@angular/material/dialog';
+import { InscriptionDialogComponent } from './components/inscription-dialog/inscription-dialog.component';
 
 @Component({
   selector: 'app-inscriptions',
@@ -15,7 +17,8 @@ export class InscriptionsComponent implements OnInit {
   displayedColumns = ['id', 'student', 'course'];
   inscriptions$: Observable<IInscription[]>;
 
-  constructor(private store: Store) {
+  constructor(private store: Store,
+              private inscriptionDialog: MatDialog) {
     this.inscriptions$ = this.store.select(selectInscriptions);
   }
 
@@ -23,4 +26,21 @@ export class InscriptionsComponent implements OnInit {
     this.store.dispatch(InscriptionActions.loadInscriptions());
   }
 
+  openDialog(){
+    this.inscriptionDialog
+      .open(InscriptionDialogComponent, {panelClass: 'inscription__dialog__panel'})
+      .afterClosed()
+      .subscribe({
+        next : (student) => {
+          if(student){
+            /*this.studentsService.insertStudent({
+              name: student.name,
+              email: student.email,
+              surname: student.surname,
+              birth: student.birth
+            })*/
+          }
+        }
+      });
+  }
 }
