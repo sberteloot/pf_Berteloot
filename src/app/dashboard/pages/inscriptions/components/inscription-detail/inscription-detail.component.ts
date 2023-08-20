251525503c2591d20de2b9af40e76120b9c7d95e
CENTRAL\sberteloot <sberteloot@coto.com.ar>
2023-08-20 19:21:51 -0300

    Quito validacion que funciona mal, reemplazo por no mostrar undifined

--- src/app/dashboard/pages/inscriptions/components/inscription-detail/inscription-detail.component.ts
+++ src/app/dashboard/pages/inscriptions/components/inscription-detail/inscription-detail.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NotifierService } from 'src/app/core/services/notifier.service';
 import { Store } from '@ngrx/store';
 import { InscriptionActions } from '../../store/inscription.actions';
-import { selectInscriptionDetailCourse, selectInscriptionDetailExists, selectInscriptionDetailtStudent } from '../../store/inscription.selectors';
+import { selectInscriptionDetailCourse, selectInscriptionDetailtStudent } from '../../store/inscription.selectors';
 import { Observable, take } from 'rxjs';
 
 @Component({
@@ -15,7 +15,6 @@ export class InscriptionDetailComponent implements OnInit {
 
   student$: Observable<string | undefined>;
   course$: Observable<string | undefined>;
-  detailExists$: Observable<boolean>;
   id:number = 0;
 
     constructor(private store: Store,
@@ -24,7 +23,6 @@ export class InscriptionDetailComponent implements OnInit {
               private notifier: NotifierService){    
     this.student$ = this.store.select(selectInscriptionDetailtStudent);
     this.course$ = this.store.select(selectInscriptionDetailCourse);
-    this.detailExists$ = this.store.select(selectInscriptionDetailExists);
   }
 
   ngOnInit(): void {
@@ -34,16 +32,6 @@ export class InscriptionDetailComponent implements OnInit {
     } else {
       this.id = Number(this.activatedRoute.snapshot.params['id']);      
       this.store.dispatch(InscriptionActions.loadInscriptionDetail({id:this.id}));
-
-      this.detailExists$.pipe(take(1)).subscribe({
-        next : (existe) => {
-          if(!existe) {
-            this.notifier.showError("No se encontró la inscripción con el Id " + this.id);
-            this.router.navigate(['dashboard', 'inscriptions']);
-          } 
-        }
-      })
-
     }        
   }
 
--- src/app/dashboard/pages/inscriptions/store/inscription.selectors.ts
+++ src/app/dashboard/pages/inscriptions/store/inscription.selectors.ts
@@ -14,5 +14,3 @@ export const selectCourses = createSelector(selectInscriptionState, (state) => s
 export const selectInscriptionDetailtStudent = createSelector(selectInscriptionState, (state) => state.inscriptionDetail?.student !== undefined ? state.inscriptionDetail?.student.name + ' ' + state.inscriptionDetail?.student.surname : ' ')
 
 export const selectInscriptionDetailCourse  = createSelector(selectInscriptionState, (state) => state.inscriptionDetail?.course.name)
-
-export const selectInscriptionDetailExists  = createSelector(selectInscriptionState, (state) => state.inscriptionDetail !== null)
